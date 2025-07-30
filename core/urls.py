b90c112b9a07d870e3b7de89337589e5f036ee16
hacmatac <marbledr@proton.me>
2025-07-30 12:11:12 -0400

    Add redirect from / to /accounts/login

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import DashboardView, download_job_json
+from django.views.generic.base import RedirectView
 
 app_name = 'core'
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name='accounts:login', permanent=False)),
     path("dashboard/", DashboardView.as_view(), name='dashboard'),
     path("dashboard/download_job_json/<int:job_id>/", download_job_json, name="download_job_json"),
 ]
